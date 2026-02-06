a0409bd835e66f0f73a391a636b1b5b6d3eb75e8
Christine Bersano <christinebersano@github.com>
2026-02-06 22:12:36 +0800

    added dummy scan history page

--- product-scanner-capstone/app/lib/main.dart
+++ product-scanner-capstone/app/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'advertise_page.dart';
 import 'register_page.dart';
 import 'login_page.dart';
 import 'home_page.dart';
 import 'forgotpassword_page.dart';
 import 'user_profile.dart';
-import 'jewel_scan_page_yolo.dart';  
+import 'jewel_scan_page_yolo.dart';
+import 'scan_history.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,9 +23,11 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
+    final supabase = Supabase.instance.client;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ProductScan',
@@ -34,7 +38,22 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      initialRoute: '/advertise',
+
+      home: FutureBuilder<bool>(
+        future: _getInitialPage(supabase),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          final bool showAdvertise = snapshot.data ?? true;
+          return showAdvertise ? const AdvertisePage() : const LoginPage();
+        },
+      ),
       routes: {
         '/advertise': (context) => const AdvertisePage(),
         '/login': (context) => const LoginPage(),
@@ -43,7 +62,31 @@ class MyApp extends StatelessWidget {
         '/forgot-password': (context) => const ForgotPasswordPage(),
         '/user_profile': (context) => UserProfilePage(),
         '/scan': (context) => const JewelScanPage(),
+        '/history': (context) => const ScanHistoryPage(),
       },
     );
+  }
+
+  /// Determines whether to show AdvertisePage
+  Future<bool> _getInitialPage(SupabaseClient supabase) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    // Check if user already accepted terms
+    final acceptedTerms = prefs.getBool('accepted_terms') ?? false;
+
+    if (!acceptedTerms) {
+      // User has not accepted terms (show AdvertisePage)
+      return true;
+    }
+
+    // User already accepted terms (check if logged in)
+    final session = supabase.auth.currentSession;
+    if (session != null) {
+      // Logged in (go to HomePage)
+      return false;
+    }
+
+    // Not logged in (go to LoginPage)
+    return false;
   }
 }
